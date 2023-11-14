07bfdd91ef8b9d86ba0f1b8c4404fe37e45eac5f
Pano Valdez <marianovaldez92@protonmail.com>
2023-11-14 20:41:28 -0300

    layout

--- src/components/withFooter/index.jsx
+++ src/components/withFooter/index.jsx
@@ -7,6 +7,7 @@ const WithFooter = ({ children, content, color }) => <div className="with-footer
   <div className="with-footer-content" >
     {children}
   </div>
+  <div className="footer"></div>
 </div>
 
 export default WithFooter;
